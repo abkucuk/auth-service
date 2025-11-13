c580224b2c48cc6a0ad25cae7b5782ddc27b1c75
ahmetbkucuk <ahmetbkucuk@gmail.com>
2025-11-13 15:22:39 +0300

    add User and Role classes for authentication model

---
+++ src/main/java/com/bankcase/auth_service/model/User.java
@@ -0,0 +1,25 @@
+package com.bankcase.auth_service.model;
+
+import com.bankcase.auth_service.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
---
+++ src/main/java/com/bankcase/auth_service/model/enums/Role.java
@@ -0,0 +1,6 @@
+package com.bankcase.auth_service.model.enums;
+
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
