b8584f76179bf548f1705ef7fb11ae5de7c0a313
ahmetbkucuk <ahmetbkucuk@gmail.com>
2025-11-13 15:23:20 +0300

    add OpenApiConfig for API documentation and JWT security scheme

---
+++ src/main/java/com/bankcase/auth_service/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.bankcase.auth_service.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components().addSecuritySchemes("BearerAuth",
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Auth Service API")
+                        .version("1.0.0")
+                        .description("JWT authentication and user management for Loan Microservices"));
+    }
+
+}
